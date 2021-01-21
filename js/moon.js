33a5364f74bd2d4be32aa6ec9301afb351e08f07
peini93 <mei319319319@gmail.com>
2021-01-21 20:38:04 +0800

    fix_00

--- js/moon.js
+++ js/moon.js
@@ -1,40 +1,14 @@
-var isMobile = 0;
-
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-	isMobile = 1;
-} else {
-	isMobile = 0;
-}
-
 $(function () {
-		$('body').jpreLoader({
-			splashID: "#jSplash",
-			loaderVPos: '50%',
-			splashVPos: '50%',
-			autoClose: true
-		}, function () { });
+		// $('body').jpreLoader({
+		// 	splashID: "#jSplash",
+		// 	loaderVPos: '50%',
+		// 	splashVPos: '50%',
+		// 	autoClose: true
+		// }, function () { });
 
 	$('.hb a').on('click', function () {
 		$(".hb a").toggleClass("change");
 		// $(".m_m").slideToggle("slow");
 		$(".m_m").stop(true, false).animate({ width: "toggle" }, 300);
 	});
-
-
-	GOTOP();
-
 });
-
-function GOTOP() {
-	$('.gotop,.logo').on("click", function () {
-		$('html,body').animate({ scrollTop: 0 }, 500);
-	});
-	$(window).scroll(function () {
-		var Y = $(window).scrollTop();
-		if (Y > 800) {
-			$(".gotop").stop(true, false).animate({ bottom: 3 + "%", opacity: 1 }, 500);
-		} else {
-			$(".gotop").stop(true, false).animate({ bottom: 0, opacity: 0 }, 500);
-		}
-	});
-}

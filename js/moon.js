6e88559447722377de7b37fde16b0b239d4e820e
peini93 <mei319319319@gmail.com>
2021-01-24 00:45:13 +0800

    fix

--- js/moon.js
+++ js/moon.js
@@ -1,3 +1,12 @@
+var isMobile = 0;
+
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+	isMobile = 1;
+} else {
+	isMobile = 0;
+}
+
+
 $(function () {
 		$('body').jpreLoader({
 			splashID: "#jSplash",
@@ -44,13 +53,11 @@ $(function () {
 						prevEl: '.swiper-button-prev',
 					}
 				});
-
-
 		});
-
 	$('.hb a').on('click', function () {
 		$(".hb a").toggleClass("change");
 		// $(".m_m").slideToggle("slow");
 		$(".m_m").stop(true, false).animate({ width: "toggle" }, 300);
 	});
 });
+
